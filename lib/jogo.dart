8b2067e5b63ee5f08a5b3470fc69459427301daa
Victor323232 <victorhugoabc59@gmail.com>
2022-04-17 19:03:34 -0300

    Estrtura do app

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -10,6 +10,24 @@ class jogo extends StatefulWidget {
 class _jogoState extends State<jogo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: "JokenPo",
+        backgroundColor: Colors.blue,
+      ),
+
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+
+          )
+        )
+      ),
+
+
+
+
+    );
   }
 }
